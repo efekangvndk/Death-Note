93557d465b56eceab54768636e7ff5ee94103bb2
Efekan.Gvndk <e.gvndk00@gmail.com>
2024-02-06 18:53:19 +0300

    Initial Commit

---
+++ Death Note/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Death Note
+//
+//  Created by Efekan Güvendik on 6.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
