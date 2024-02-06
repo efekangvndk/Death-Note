1d1c149e78e1821ba3385cd47ab71e699cc57227
Efekan.Gvndk <e.gvndk00@gmail.com>
2024-02-07 01:39:43 +0300

    Creat Button and setting.

--- Death Note/ViewController.swift
+++ Death Note/ViewController.swift
@@ -15,21 +15,32 @@ class ViewController: UIViewController {
         self.view.backgroundColor = .white
         
         
-        /// LABELS----------------
+        /// VARS------------------
         let baslikLabel = UILabel()
+        let addButton = UIButton(type: .system)
         
-        /// TEXTS-----------------
+        /// LABEL SETTİNGS---------
         baslikLabel.text = "DEATH NOTE"
         baslikLabel.textAlignment = .center
-        
-        /// SİZE AND LOCATİONS----
-        baslikLabel.frame = CGRect(x: 0 , y:0, width: 200, height: 200)
+        baslikLabel.font = UIFont.systemFont(ofSize: 35)
+        baslikLabel.frame = CGRect(x: 100 , y:100, width: 300, height: 300)
         baslikLabel.center = CGPoint(x: view.frame.size.width / 2, y: 100)
-        
         baslikLabel.textColor = .black
-        self.view.addSubview(baslikLabel)
         
-        // Do any additional setup after loading the view.
+        
+        /// OTHERS SETTİNGS--------
+        let setImage = UIImage.SymbolConfiguration(pointSize: 40,weight: .medium)
+        let addImage = UIImage(systemName: "plus.app", withConfiguration: setImage)
+        addButton.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+        addButton.center = CGPoint(x: 385, y: 800)
+        addButton.setImage(addImage, for: .normal)
+        addButton.tintColor = .black
+        
+        
+        /// ADD TO VİEW-------------
+        self.view.addSubview(baslikLabel)
+        self.view.addSubview(addButton)
+        
     }
 
 
