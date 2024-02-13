b1abcadb827de4d604fd5ef0a088782a49c9d243
Efekan.Gvndk <e.gvndk00@gmail.com>
2024-02-13 23:32:32 +0300

    Update SecondViewController.swift

--- Death Note/SecondViewController.swift
+++ Death Note/SecondViewController.swift
@@ -30,7 +30,7 @@ class SecondViewController: UIViewController, UITextFieldDelegate {
         note.frame = CGRect(x: 0, y: 0, width: 300 , height: 35)
         note.center = CGPoint(x: view.frame.width / 2, y: 150)
         note.layer.cornerRadius = 20
-        note.layer.borderWidth = 1.1
+        note.layer.borderWidth = 1.0
         note.layer.borderColor = UIColor.systemRed.cgColor
         note.layer.shadowColor = UIColor.black.cgColor
         note.layer.shadowOpacity = 3.00
