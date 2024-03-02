0dde9c7a49815d26d3b2b639574a6b1046193393
Efekan.Gvndk <e.gvndk00@gmail.com>
2024-03-02 18:44:17 +0300

    nnn

--- Death Note/SecondViewController.swift
+++ Death Note/SecondViewController.swift
@@ -1,17 +1,10 @@
-//
-//  secViewController.swift
-//  Death Note
-//
-//  Created by Efekan Güvendik on 9.02.2024.
-//
-
 import UIKit
 
 class SecondViewController: UIViewController, UITextFieldDelegate {
        
     
     /// VARS------------------
-    var note = UITextField()                                                                //oluşturulan değişkenler
+    var note = UITextField()                                                                // Oluşturulan değişkenler
     let baslikLabel2 = UILabel()
     
     
@@ -29,28 +22,34 @@ class SecondViewController: UIViewController, UITextFieldDelegate {
         note.font = UIFont(name: "Helvetica", size: 15)
         note.layer.cornerRadius = 20
         note.layer.borderWidth = 1.0
-        note.layer.borderColor = UIColor.systemRed.cgColor
+        note.layer.borderColor = UIColor.systemRed.cgColor                                  // cgColor düşük seviye apiler içindir.
+        note.layer.shadowRadius = 10
+        note.layer.shadowColor = UIColor.black.cgColor
         
-            NSLayoutConstraint.activate([
-                
-                ])
+        self.view.addSubview(note)                                                          // view'e note'u ekliyoruz
+        note.translatesAutoresizingMaskIntoConstraints = false
         
-        /// ADD TO VİEW-----------
-        self.view.addSubview(note)
+        NSLayoutConstraint.activate([
+            note.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 150),
+            note.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            note.widthAnchor.constraint(equalToConstant: 300),
+            note.heightAnchor.constraint(equalToConstant: 35)
+        ])
     }
     
     
     func textFieldDidBeginEditing(_ textField : UITextField){
-        UIView.animate(withDuration: 1.0){
-            self.note.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+            // Düzenleme moduna başladığında animasyon uygula
+            UIView.animate(withDuration: 1.0){
+                self.note.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+            }
+        }
+        
+        
+        func textFieldDidEndEditing(_ textField: UITextField) {
+            // Düzenleme modundan çıkıldığında animasyonu geri al
+            UIView.animate(withDuration: 1.0){
+                self.note.transform = CGAffineTransform.identity
+            }
         }
     }
-    
-    
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        UIView.animate(withDuration: 1.0){
-            self.note.transform = CGAffineTransform.identity
-        }
-    }
-   
-}
