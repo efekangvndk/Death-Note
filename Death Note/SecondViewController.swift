fb5dc84ee0931a53101a9c12087d7e88453e224e
Efekan.Gvndk <e.gvndk00@gmail.com>
2024-03-02 23:13:49 +0300

    textfield to textview

--- Death Note/SecondViewController.swift
+++ Death Note/SecondViewController.swift
@@ -1,10 +1,10 @@
 import UIKit
 
-class SecondViewController: UIViewController, UITextFieldDelegate {
-       
+class SecondViewController: UIViewController, UITextFieldDelegate , UITextViewDelegate{
+    
     
     /// VARS------------------
-    var note = UITextField()                                                                // Oluşturulan değişkenler
+    var note = UITextView()                                                                // Oluşturulan değişkenler
     let baslikLabel2 = UILabel()
     
     
@@ -15,9 +15,15 @@ class SecondViewController: UIViewController, UITextFieldDelegate {
         
         /// LABEL SETTİNGS-------
         baslikLabel2.text = "RULES"
+        baslikLabel2.textColor = .black
         
+        baslikLabel2.frame = CGRect(x: 100 , y:100, width: 300, height: 300)
+        baslikLabel2.center = CGPoint(x: view.frame.size.width / 2, y: 100)
         
         /// TEXTFİELD SETTİNGS---
+        note.text = "WHO WİLL KİLL BE"
+        note.textContainerInset = UIEdgeInsets(top: 8, left: 50, bottom: 8, right: 8)
+        note.textColor = UIColor.lightGray
         note.backgroundColor = .white
         note.font = UIFont(name: "Helvetica", size: 15)
         note.layer.cornerRadius = 20
@@ -26,30 +32,51 @@ class SecondViewController: UIViewController, UITextFieldDelegate {
         note.layer.shadowRadius = 10
         note.layer.shadowColor = UIColor.black.cgColor
         
+        /*
+          BU KISIM TEXTFİEL İÇİN YAZILDI
+         let leftPaddingView = UIView(frame: CGRect(x: 0, y: 0, width: 20, height: 20))      // Padding oluşturup ona view yani bir görünüm veriyoruz.
+         note.leftView = leftPaddingView                                                     // Oluşturduğumuz padding için konumu sol tarafa çekiyoruz.
+         note.leftViewMode = .always                                                         // Görünümün hep orda oluşması
+         */
+        
+        
+        self.view.addSubview(baslikLabel2)
         self.view.addSubview(note)                                                          // view'e note'u ekliyoruz
         note.translatesAutoresizingMaskIntoConstraints = false
+        
         
         NSLayoutConstraint.activate([
             note.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 150),
             note.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
             note.widthAnchor.constraint(equalToConstant: 300),
-            note.heightAnchor.constraint(equalToConstant: 35)
+            note.heightAnchor.constraint(equalToConstant: 200)
         ])
+        
+        note.delegate = self
+        
     }
     
     
-    func textFieldDidBeginEditing(_ textField : UITextField){
-            // Düzenleme moduna başladığında animasyon uygula
-            UIView.animate(withDuration: 1.0){
-                self.note.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
-            }
+    
+    // Büyüme animasyon ekleniyor.
+    func textViewDidBeginEditing(_ textView: UITextView){
+        if note.textColor == UIColor.lightGray {
+            note.text = nil
+            note.textColor = UIColor.black
         }
-        
-        
-        func textFieldDidEndEditing(_ textField: UITextField) {
-            // Düzenleme modundan çıkıldığında animasyonu geri al
-            UIView.animate(withDuration: 1.0){
-                self.note.transform = CGAffineTransform.identity
-            }
-        }
+        UIView.animate(withDuration: 1.2, animations: {
+            textView.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+        })
     }
+    
+    // Düzenleme modundan çıkıldığında animasyonu geri al
+    func textViewDidEndEditing(_ textView : UITextView) {
+        if note.text.isEmpty {
+            note.text = "WHO WİLL KİLL BE"
+            note.textColor = UIColor.lightGray
+        }
+           UIView.animate(withDuration: 1.0, animations: {
+               textView.transform = CGAffineTransform.identity    // Normal boyua geri dönüş.
+           })
+       }
+   }
