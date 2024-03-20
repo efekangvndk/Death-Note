188bc24d46c0100d90b2b4e858c7f81178cdfacc
Efekan.Gvndk <e.gvndk00@gmail.com>
2024-03-20 05:38:09 +0300

    nn

--- Death Note/Helpers/UIComponentHelper.swift
+++ Death Note/Helpers/UIComponentHelper.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 struct UIComponentsHelper {
-    static func createCustomLabel(text : String , size :CGFloat, labelBackGroundColor : UIColor , textColor : UIColor , fontName : String , cornerRadius : CGFloat, borderWidth : CGFloat, borderColor : CGColor ) -> UILabel {
+    static func createCustomLabel(text : String , size :CGFloat, labelBackGroundColor : UIColor , textColor : UIColor , fontName : String , cornerRadius : CGFloat, borderWidth : CGFloat, borderColor : CGColor ,addshadow : Bool) -> UILabel {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = text
@@ -16,24 +16,36 @@ struct UIComponentsHelper {
         label.layer.cornerRadius = cornerRadius
         label.layer.borderWidth = borderWidth
         label.layer.borderColor = borderColor
-        label.clipsToBounds = true
         label.backgroundColor = labelBackGroundColor
         label.textAlignment = .center
         label.textColor = textColor
         label.numberOfLines = 2
+        label.clipsToBounds = true
+        if addshadow {
+            label.layer.shadowColor = UIColor.black.cgColor
+            label.layer.shadowOffset = CGSize(width: 20, height: 10)
+            label.layer.shadowRadius = 10
+            label.layer.shadowOpacity = 5
+        }
         return label
     }
-        static func creatCustomButton(ButtonName : String , tintColor : UIColor, image : UIImage , cornerRadius : CGFloat , borderWidth : CGFloat,borderColor : CGColor,backgroundColor : UIColor)-> UIButton{
+    static func creatCustomButton(ButtonName : String , tintColor : UIColor, image : UIImage? , cornerRadius : CGFloat , borderWidth : CGFloat,borderColor : CGColor,backgroundColor : UIColor, addshadow : Bool)-> UIButton{
             let button = UIButton()
             button.translatesAutoresizingMaskIntoConstraints = false
             button.setTitle(ButtonName , for: .normal)
-            button.tintColor = tintColor
+            button.setTitleColor(tintColor, for: .normal) 
             button.setImage(image, for: .normal)
             button.layer.cornerRadius = cornerRadius
             button.layer.borderWidth = borderWidth
             button.layer.borderColor = borderColor
             button.backgroundColor = backgroundColor
-            return button
         
+        if addshadow {
+            button.layer.shadowColor = UIColor.black.cgColor
+            button.layer.shadowOffset = CGSize(width: 0, height: 2)
+            button.layer.shadowRadius = 10
+            button.layer.shadowOpacity = 5
+        }
+            return button
     }
 }
--- Death Note/Views/Home/HomeView.swift
+++ Death Note/Views/Home/HomeView.swift
@@ -9,8 +9,8 @@ import UIKit
 
 class HomeView: UIView {
     
-    let title = UIComponentsHelper.createCustomLabel(text: "The Note", size: 50 , labelBackGroundColor: .red , textColor: .white, fontName: "The Valentine", cornerRadius: 10 , borderWidth: 2, borderColor: UIColor.yellow.cgColor)
-    let addbutton = UIComponentsHelper.creatCustomButton(ButtonName: "Take a note", tintColor: .red , image: , cornerRadius: 20, borderWidth: <#T##CGFloat#>, borderColor: <#T##CGColor#>, backgroundColor: <#T##UIColor#>)
+    let title = UIComponentsHelper.createCustomLabel(text: "The Note", size: 50 , labelBackGroundColor: .red , textColor: .white, fontName: "The Valentine", cornerRadius: 10 , borderWidth: 2, borderColor: UIColor.yellow.cgColor, addshadow: true)
+    let addbutton = UIComponentsHelper.creatCustomButton(ButtonName: "Take a note", tintColor: .white , image: nil , cornerRadius: 5, borderWidth: 2, borderColor: UIColor.yellow.cgColor , backgroundColor: .red, addshadow: true)
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -35,8 +35,13 @@ class HomeView: UIView {
             title.widthAnchor.constraint(equalToConstant: 120),
             title.heightAnchor.constraint(equalToConstant: 50),
             title.centerXAnchor.constraint(equalTo: centerXAnchor),
+            addbutton.topAnchor.constraint(equalTo: topAnchor, constant: 850),
+            addbutton.centerXAnchor.constraint(equalTo: centerXAnchor),
+            addbutton.widthAnchor.constraint(equalToConstant: 120),
+            addbutton.heightAnchor.constraint(equalToConstant: 30),
         ])
     }
+    
     
     
     
