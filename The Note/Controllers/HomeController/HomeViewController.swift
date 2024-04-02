c95c226fa093ae50af549d42f92634a968dfe2e5
Efekan.Gvndk <e.gvndk00@gmail.com>
2024-04-02 21:05:03 +0300

    Trying to be Layot

--- The Note/Controllers/HomeController/HomeViewController.swift
+++ The Note/Controllers/HomeController/HomeViewController.swift
@@ -11,77 +11,54 @@ import CoreData
 
 class HomeViewController: UIViewController , UITableViewDelegate, UITableViewDataSource {
     
-    private var homeView = HomeView()
+     var homeViewController = HomeView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        homeView = HomeView(frame: view.bounds)
-        view.addSubview(homeView)
+        homeViewController = HomeView(frame: view.bounds)
+        view.addSubview(homeViewController)
         setupController()
     }
     private func setupController() {
         title = "Home"
         navigationController?.navigationBar.tintColor = .systemRed
-        homeView.addbutton.addTarget(self, action: #selector(addButtonDown), for: .touchDown)
-        homeView.addbutton.addTarget(self, action: #selector(addButtonUp), for: [.touchUpInside , .touchUpOutside])
-        homeView.tableView.delegate = self
-        homeView.tableView.dataSource = self
+        homeViewController.addbutton.addTarget(self, action: #selector(addButtonDown), for: .touchDown)
+        homeViewController.addbutton.addTarget(self, action: #selector(addButtonUp), for: [.touchUpInside , .touchUpOutside])
+        homeViewController.tableView.delegate = self
+        homeViewController.tableView.dataSource = self
         getData()
     }
-    
-    func getData(){
-        
-        let appDelegate = UIApplication.shared.delegate as? AppDelegate
-        let context = appDelegate?.persistentContainer.viewContext
-        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Nots")
-        fetchRequest.returnsObjectsAsFaults = false
-        
-        do {
-            if let context = context{
-                let results = try context.fetch(fetchRequest)
-                
-                for result in results as! [NSManagedObject]{
-                    if let nots = result.value(forKey: "nots") as? String{
-                        homeView.nots.append(nots)
-                    }
-                }
-            }else{
-                print("Error")
-            }
-            
-            homeView.tableView.reloadData()
-            
-        }catch{
-            print("Error")
-        }
-        
-        
-    }
+    ///
+    ///
+    ///
+    ///
+    ///
+
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return homeView.nots.count
+        return homeViewController.nots.count
         
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = homeView.nots[indexPath.row]
+        cell.textLabel?.text = homeViewController.nots[indexPath.row]
         return cell
     }
     
     @objc func addButtonDown(){
         UIView.animate(withDuration: 0.2 ){
-            self.homeView.addbutton.transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
+            self.homeViewController.addbutton.transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
         }
-        homeView.addbutton.alpha = 0.5
-        homeView.addbutton.backgroundColor = UIColor.black
+        homeViewController.addbutton.alpha = 0.5
+        homeViewController.addbutton.backgroundColor = UIColor.black
     }
     @objc func addButtonUp() {
         UIView.animate(withDuration: 0.2) {
-            self.homeView.addbutton.transform = CGAffineTransform.identity
+            self.homeViewController.addbutton.transform = CGAffineTransform.identity
         }
-        homeView.addbutton.alpha = 1.0
-        homeView.addbutton.backgroundColor = UIColor.red
+        homeViewController.addbutton.alpha = 1.0
+        homeViewController.addbutton.backgroundColor = UIColor.red
         let VC = NotesViewController()
         navigationController?.pushViewController(VC, animated: true)
     }
