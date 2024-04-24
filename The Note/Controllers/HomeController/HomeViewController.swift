0e6920003b34182d7f8fb128740e9ec96b2b24fb
Efekan.Gvndk <76440360+efekangvndk@users.noreply.github.com>
2024-04-24 04:03:31 +0300

    button

--- The Note/Controllers/HomeController/HomeViewController.swift
+++ The Note/Controllers/HomeController/HomeViewController.swift
@@ -1,10 +1,10 @@
 import UIKit
 import CoreData
 
-class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var homeControllerView: HomeView!
-    var titleNoteView: [String] = []
+    var titleNoteView: [NSManagedObject] = []
     
     
     override func viewDidLoad() {
@@ -41,14 +41,7 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         do {
             let results = try context.fetch(fetchRequest)
-            titleNoteView = []
-            for result in results as! [NSManagedObject] {
-                if let cellTitle = result.value(forKey: "title") as? String {
-                    titleNoteView.append(cellTitle)
-                    print(titleNoteView)
-
-                }
-            }
+            titleNoteView = results as! [NSManagedObject]
             homeControllerView.tableView.reloadData()
         } catch {
             print("Error fetching data: \(error)")
@@ -60,18 +53,22 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        cell.textLabel?.text = titleNoteView[indexPath.row]
-        
-        
-        // MARK: TableView Edith Button işlemleri.
-        let edithButton = UIButton(type: .system)
-        edithButton.setTitle("+", for: .highlighted)
-        edithButton.tintColor = UIColor.black
-        edithButton.addTarget(self, action: #selector(editButtonTapped(_:)), for: .touchUpInside)
-        cell.accessoryView = edithButton
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        let note = titleNoteView[indexPath.row]
+        cell.textLabel?.text = note.value(forKey: "title") as? String
+
+        // Hücreye buton eklemek
+        let editButton = UIButton(type: .system)
+        editButton.setTitle("+", for: .normal)
+        editButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 15)
+        editButton.setTitleColor(UIColor.blue, for: .normal)
+        editButton.addTarget(self, action: #selector(editButtonTapped(_:)), for: .touchUpInside)
+        editButton.frame = CGRect(x: cell.frame.width - 10, y: 5, width: 60, height: 34)
+        cell.contentView.addSubview(editButton)
+
         return cell
     }
+
     
     @objc func editButtonTapped(_ sender: UIButton) {
         let edithVC = EdithViewController()
@@ -102,20 +99,20 @@ class HomeViewController: UIViewController, UITableViewDelegate, UITableViewData
         if editingStyle == .delete{ // kaydırarak silme işlemi. y
             let appDelegate = UIApplication.shared.delegate as? AppDelegate
             let context = appDelegate?.persistentContainer.viewContext
-            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Nots")
+            let objectToDelete = titleNoteView[indexPath.row]
            
             do{
-                let result = try context?.fetch(fetchRequest)
-                if let object = result as?[NSManagedObject]{
-                    let objectToDelete = object[indexPath.row]
-                    context?.delete(objectToDelete)
-                    try context?.save()
-                    titleNoteView.remove(at: indexPath.row)
-                    tableView.deleteRows(at: [indexPath], with: .fade)
-                }
+                context?.delete(objectToDelete)
+                try context?.save()
+                titleNoteView.remove(at: indexPath.row)
+                tableView.deleteRows(at: [indexPath], with: .fade)
             }catch{
                 print("Error deleting item : \(error)")
             }
         }
     }
 }
+
+
+// MARK:  edith screen yapılacak ve özellikler düzenlenecek...
+
